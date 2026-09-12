a4dc139355308ce39ddc75efe5cacbcd7e969f67
Diba Darooneh <133037571+Dibadi1384@users.noreply.github.com>
2026-09-11 21:08:45 -0400

    Time: 248 ms (45.72%) | Memory: 21.6 MB (72.94%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,30 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        islands=0
 
-        numisl=0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c]=="1":
+                    self.dfs(grid,r,c)
+                    islands+=1
+    
 
-        for i in range (len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j]=="1":
-                    self.dfs(grid, i,j)
-                    numisl+=1
-        return numisl
+        return islands
 
+    
     def dfs(self, grid, r, c):
-        #mark as visited 
-
+        #first mark as visited
         grid[r][c]="0"
+
         dirs=[(0,1), (0,-1), (1,0), (-1,0)]
         
-        # we are checking for every cell so time complexity is O(mn)
         for dirc in dirs:
-            i=r+dirc[0]
-            j=c+dirc[1]
+            i,j=r+dirc[0],c+dirc[1]
 
-            if(i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]=="0"):
-                continue 
-            self.dfs(grid, i,j)
+            #if out of bound or zero
+            if (i<0 or j<0 or i>len(grid)-1 or j>len(grid[0])-1 or grid[i][j]=="0"):
+                continue
             
-
+            #otherwise if one do a dfs on that node as a root
+            self.dfs(grid,i,j)
+        
